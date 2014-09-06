5a1b3766a7c34ee47fe03ab51064fba5ccc2194e
kecheon <kecheon@gmail.com>
2014-09-06 19:14:14 +0900

    홈페이지 헤더 추가

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+#-*-coding:utf8-*-
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world!"
+    return u"InnoMVA: 주식회사 이노지투비"
 
 if __name__ == "__main__":
     app.run()
