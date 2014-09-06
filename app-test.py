dac40fd2cd6d7b49099033d9dc68f365ed27877b
kecheon <kecheon@gmail.com>
2014-09-06 22:47:06 +0900

    boostrap 적용

--- app-test.py
+++ app-test.py
@@ -1,6 +1,6 @@
 #-*-coding:utf8-*-
 from app import app
-from flask import Flask, template_rendered
+from flask import Flask, template_rendered, url_for
 import unittest
 from flask.ext.testing import TestCase
 
@@ -14,13 +14,17 @@ class BasicTestCase(TestCase):
     def test_landing_page(self):
         response = self.client.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, u"InnoMVA: 주식회사 이노지투비")
+        self.assertIn(u"InnoMVA: 주식회사 이노지투비", response.data)
 
     # user가 우리 사이트를 허섭스러기로 생각한다.
     # 제일 어렵지만 디자인을 입히자.
     def test_use_template(self):
         response = self.client.get('/')
         self.assert_template_used('home.html')
+
+    def test_apply_css(self):
+        response = self.client.get(url_for('static', filename='custom.css'))
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 #-*-coding:utf8-*-
 
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return u"InnoMVA: 주식회사 이노지투비"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run()
